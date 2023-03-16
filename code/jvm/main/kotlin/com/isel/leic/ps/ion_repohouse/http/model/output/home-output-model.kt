eeb6acc692797ba62abef454f706f96c05b43a1d
Henriquess19 <ricardo.freitas.henriques@gmail.com>
2023-03-16 17:29:55 +0000

    Added proposal

---
+++ code/jvm/main/kotlin/com/isel/leic/ps/ion_repohouse/http/model/output/home-output-model.kt
@@ -0,0 +1,7 @@
+package com.isel.leic.ps.ion_repohouse.http.model.output
+
+data class HomeOutputModel(
+    val title:String = "i-on ClassCode",
+    val description:String = "The web application from students to teachers and students.",
+    val est :String = "2023",
+):OutputModel
