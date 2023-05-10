7085498e449903da8e07566932c2e34ae820799c
Henriquess19 <ricardo.freitas.henriques@gmail.com>
2023-05-10 17:55:23 +0100

    Web testing

--- code/jvm/main/kotlin/com/isel/leic/ps/ionClassCode/http/model/output/CreditsOutputModel.kt
+++ code/jvm/main/kotlin/com/isel/leic/ps/ionClassCode/http/model/output/CreditsOutputModel.kt
@@ -4,12 +4,12 @@ package com.isel.leic.ps.ionClassCode.http.model.output
  * Represents a Credits Output Model.
  */
 data class CreditsOutputModel(
-    val teacher: CreditsTeacher = CreditsTeacher("Pedro Félix", "pedro.felix@isel.pt","https://github.com/pmhsfelix"),
+    val teacher: CreditsTeacher = CreditsTeacher("Pedro Félix", "pedro.felix@isel.pt", "https://github.com/pmhsfelix"),
     val students: List<CreditsStudent> = listOf<CreditsStudent>(
-        CreditsStudent("André Santos", 48309, "A48309@alunos.isel.pt","https://github.com/AndreSantos0"),
-        CreditsStudent("Ricardo Henriques", 48322, "A48322@alunos.isel.pt","https://github.com/Henriquess19"),
-        CreditsStudent("João Magalhães", 48323, "A48348@alunos.isel.pt","https://github.com/JoaoMagalhaes23"),
-    )
+        CreditsStudent("André Santos", 48309, "A48309@alunos.isel.pt", "https://github.com/AndreSantos0"),
+        CreditsStudent("Ricardo Henriques", 48322, "A48322@alunos.isel.pt", "https://github.com/Henriquess19"),
+        CreditsStudent("João Magalhães", 48348, "A48348@alunos.isel.pt", "https://github.com/JoaoMagalhaes23"),
+        ),
 ) : OutputModel
 
 /**
@@ -19,7 +19,7 @@ data class CreditsStudent(
     val name: String,
     val schoolNumber: Int,
     val email: String,
-    val githubLink: String
+    val githubLink: String,
 )
 
 /**
@@ -28,5 +28,5 @@ data class CreditsStudent(
 data class CreditsTeacher(
     val name: String,
     val email: String,
-    val githubLink: String
+    val githubLink: String,
 )
