afbd726d910a20affeab888ae2ef7588e6fc9aff
Henriquess19 <ricardo.freitas.henriques@gmail.com>
2023-05-26 21:17:03 +0100

    Cleanup folder

--- code/jvm/main/kotlin/com/isel/leic/ps/ionClassCode/http/GithubUrl.kt
+++ code/jvm/main/kotlin/com/isel/leic/ps/ionClassCode/http/GithubUrl.kt
@@ -36,9 +36,3 @@ val GITHUB_ACCESS_TOKEN_URI: (code: String) -> String = { code ->
 }
 
 const val GITHUB_USER_ORGS = "/user/orgs"
-val GITHUB_ORG_REPOS_URI: (orgName: String, perPage: Int, page: Int) -> String = { org, _, _ -> "/orgs/$org/repos?type=all" }
-val GITHUB_ORG_CREATE_REPO_URI: (orgName: String) -> String = { org -> "/orgs/$org/repos" }
-val GITHUB_ORG_TEAMS_URI: (orgName: String) -> String = { org -> "/orgs/$org/teams" }
-val GITHUB_ORG_TEAM_URI: (orgName: String, teamName: String) -> String = { org, team -> "/orgs/$org/teams/$team" }
-val GITHUB_ORG_TEAMS_USER_URI: (orgName: String, teamName: String, userName: String) -> String = { org, team, user -> "/orgs/$org/teams/$team/memberships/$user" }
-val GITHUB_ORG_USER_URI: (orgName: String, userName: String) -> String = { org, user -> "/orgs/$org/memberships/$user" }
